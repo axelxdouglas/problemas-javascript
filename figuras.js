e4dbd5becb2d3841406ed6e6e54d660142298530
axexdouglas <axelxdouglas@gmail.com>
2022-07-04 13:05:17 -0300

    agrupación de clgs

---
+++ figuras.js
@@ -0,0 +1,24 @@
+//Código del cuadrado
+    console.group("Cuadrados")//Agrupación de clgs
+
+const ladoCuadrado = 5;
+console.log(`Los lados del cuadrado miden: ${ladoCuadrado} cm`)
+
+const perimetroCuadrado = ladoCuadrado*4
+console.log(`El perímetro del cuadrado mide: ${perimetroCuadrado} cm`)
+
+const areaCuadrado = ladoCuadrado*ladoCuadrado
+console.log(`El área del cuadrado mide: ${areaCuadrado} cm^2`)
+    console.groupEnd();
+
+//Código del triángulo
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+console.log(
+    `Los lados del triángulo miden: 
+    ${ladoTriangulo1} cm, 
+    ${ladoTriangulo2} cm 
+    y ${baseTriangulo} cm`
+    );
+
